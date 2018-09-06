7709a75943918160f00bc7b56d4ca9463d075e92
Tu <stefan.tu@CAMPUS.fh-ludwigshafen.de>
2018-09-06 15:47:52 +0200

    add

---
+++ function.py
@@ -0,0 +1,5 @@
+# name functions in lowercase
+def say_hi(name):
+    print("Hello " + name)
+
+say_hi("Bongo")
---
+++ tuples.py
@@ -0,0 +1,3 @@
+# tuples () like below in coordinates are immutable unlike lists []
+coordinates = (4, 5)
+print(coordinates[0])
