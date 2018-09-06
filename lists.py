594d26612f7f06a8bc311a21e8c74f41016b65a8
Tu <stefan.tu@CAMPUS.fh-ludwigshafen.de>
2018-09-06 13:37:55 +0200

    expanded lists

--- lists.py
+++ lists.py
@@ -1,3 +1,15 @@
-friends = ["Kai", "Seb", "Marc"]
 
+lucky_numbers = [4, 6, 2, 99, 17, 56, 9]
+friends = ["Kai", "Seb", "Seb", "Marc"]
+friends.extend(lucky_numbers)
+friends.insert(1, "Bongo")
+friends.remove("Marc")
+friends.append("Creed")
+friends.pop()
 print(friends)
+print(friends.count("Seb"))
+print(friends.index("Seb"))
+lucky_numbers.sort()
+print(lucky_numbers)
+lucky_numbers.reverse()
+print(lucky_numbers)
